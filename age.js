a6d0e10e693cbae2e5bd1959a3019ee36ce9a97e
Rats9 <mmmrajat841996@gmail.com>
2022-12-25 00:01:43 +0530

    JS-101_Learning_Javascript/Introduction_to_Javascript

---
+++ age.js
@@ -0,0 +1,6 @@
+let name = "Rajat Mishra";
+console.log(name);
+console.log(typeof (name));
+let age = 26;
+console.log(age);
+console.log(typeof (age));
---
+++ name.js
@@ -0,0 +1,6 @@
+let name = "Rajat Mishra";
+console.log(name);
+let name1 = "AjayDutt Mishra";
+console.log(name1);
+let name2  = "Bhawana Mishra";
+console.log(name2); 
